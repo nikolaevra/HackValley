14109c669e2adab60c02db4ecd2e17dc2acb99ff
Subhan Nadeem <s5nadeem@uwaterloo.ca>
2017-01-07 18:36:16 -0500

    Optimized files

--- app/src/main/java/com/example/ruslan/chatapplication/LogInActivity.java
+++ app/src/main/java/com/example/ruslan/chatapplication/LogInActivity.java
@@ -6,9 +6,6 @@ import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
@@ -33,8 +30,7 @@ import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class LogInActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+public class LogInActivity extends AppCompatActivity {
 
     private static final String TAG = "Testing: ";
     public static final String USERNAME = "username";
@@ -61,23 +57,8 @@ public class LogInActivity extends AppCompatActivity
         // Initialize Facebook SDK & logger
         FacebookSdk.sdkInitialize(getApplicationContext());
         AppEventsLogger.activateApp(this);
-        setContentView(R.layout.activity_log_in);
-        fadeIn(findViewById(R.id.drawer_layout));
-        /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);*/
-
-
-        /*// Navigation drawer layout initialization
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();*/
-
-
-        // Navigation view layout initialization
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        setContentView(R.layout.content_log_in);
+        fadeIn(findViewById(R.id.content_log_in));
 
         // Set custom text
         Typeface myTypeface = Typeface.createFromAsset(getAssets(), "coolvetica.ttf");
@@ -90,16 +71,6 @@ public class LogInActivity extends AppCompatActivity
         }
         initializeLogInButton();
         initializeFirebaseAuthorization();
-    }
-
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
     }
 
     @Override
@@ -228,30 +199,5 @@ public class LogInActivity extends AppCompatActivity
                 // ...
             }
         };
-    }
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
     }
 }
